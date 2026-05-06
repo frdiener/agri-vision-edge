d37651c425f4308cdee2e88891636bcc73eb31c0
Freimut Diener <mail@f-diener.dev>
2026-05-06 08:42:18 +0200

    Add module initialization

---
+++ src/agri_vision_edge/__init__.py
@@ -0,0 +1,9 @@
+"""
+agri_vision_edge
+
+Utilities for agricultural computer vision workflows,
+including preprocessing, TFRecord generation,
+and TensorFlow Lite quantization support.
+"""
+
+__version__ = "0.1.0"
