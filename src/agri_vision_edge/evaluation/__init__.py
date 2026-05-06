67bc52017c3ed70eedc0283bd0a50be4a5804c78
Freimut Diener <mail@f-diener.dev>
2026-05-06 17:16:26 +0200

    evaluation: fix exports

--- src/agri_vision_edge/evaluation/__init__.py
+++ src/agri_vision_edge/evaluation/__init__.py
@@ -12,10 +12,15 @@ from .tensorboard import load_event_scalars
 from .curves import (
     plot_loss_curves,
     plot_learning_rate,
+    plot_steps_per_second,
+    available_tags,
 )
 
 __all__ = [
     "load_event_scalars",
     "plot_loss_curves",
     "plot_learning_rate",
+    "plot_steps_per_second",
+    "available_tags",
 ]
+
